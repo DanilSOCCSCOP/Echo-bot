6c2d6dbbc9329a14e697424387d3f1abde1c318e
Danil <nnaumov538@gmail.com>
2022-12-08 23:49:05 +0500

    Echo bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+import settings
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
+                    level=logging.INFO,
+                    filename = "bot.log"
+                    )
+
+def start_bot(updater: Updater, CallbackContext):
+    bottext = f"Привет {updater.message.chat.first_name}!\n\nЯ повторюшка)" 
+    updater.message.reply_text(bottext)
+
+def chat(updater: Updater, CallbackContext):
+	text = updater.message.text
+	logging.info(text)
+
+	updater.message.reply_text(text)
+
+def main():
+    updtr = Updater(settings.TOKEN_TELEGRAM)
+
+    updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+    updtr.start_polling()
+    updtr.idle()
+
+if __name__ == "__main__":
+    logging.info("Bot started")
+    main()
